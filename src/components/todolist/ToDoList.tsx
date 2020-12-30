f638cde7a1a329f6a6abdd194e944c9aacf57f22
Roger Esteve <rogeresteve30@gmail.com>
2020-12-30 23:12:42 +0100

    Search bar

--- src/components/todolist/ToDoList.tsx
+++ src/components/todolist/ToDoList.tsx
@@ -3,24 +3,36 @@ import React from 'react';
 import {Item} from './Item';
 
 interface IItem {
+    id: number,
     name: string,
     checked: boolean
 }
 
-export class ToDoList extends React.Component {
+interface State {
+    items: IItem[],
+    filteredItems: IItem[],
+    inputVal: string,
+    searchVal: string,
+}
+
+export class ToDoList extends React.Component<{}, State> {
     state = {
-        items: [],
+        items: [] as IItem[],
+        filteredItems: [] as IItem[],
         inputVal: '',
+        searchVal: '',
     };
 
     constructor(props : any) {
         super(props);
-        
+    }
+
+    componentDidMount = () => {
         let meh = localStorage.getItem('items');
         let itemsFromStorage: IItem[] = meh ? JSON.parse(meh) : null;
         if(itemsFromStorage) {
-            console.log(itemsFromStorage);
             this.setState({items: itemsFromStorage}, this.listUpdated);
+            this.setState({filteredItems: itemsFromStorage}, this.listUpdated);
         }
     }
 
@@ -28,23 +40,38 @@ export class ToDoList extends React.Component {
         e.preventDefault();
         if(this.state.inputVal === '') return;
         let arr: IItem[] = this.state.items;
-        arr.push({name: this.state.inputVal, checked: false});
+        arr.push({id: arr.length, name: this.state.inputVal, checked: false});
         this.setState({items: arr}, this.listUpdated);
         this.setInputVal('');
+        this.updateFilteredItems();
     }
 
     updateCheckItem = (i: number, checked: boolean) => {
         let arr: IItem[]  = this.state.items;
         arr[i].checked = checked;
         this.setState({items: arr}, this.listUpdated);
+        this.updateFilteredItems();
     }
 
-    setInputVal = (val: String) => {
+    searchValUpdate = async (val: string) => {
+        console.log('1Val: ', val)
+        await this.setState({searchVal: val});
+        console.log('2Val: ', this.state.searchVal)
+        this.updateFilteredItems();
+    }
+
+    setInputVal = (val: string) => {
         this.setState({inputVal: val});
     }
 
+    updateFilteredItems = () => {
+        const filtered: IItem[] = this.state.items.filter((item: IItem) => {
+            return item.name.toLowerCase().includes(this.state.searchVal);
+        });
+        this.setState({filteredItems: filtered});
+    }
+
     listUpdated = () => {
-        console.log("vasdfasdfas");
         let todo: IItem[]  = [];
         let done: IItem[]  = [];
         this.state.items.map((item: IItem) => {
@@ -53,7 +80,7 @@ export class ToDoList extends React.Component {
             return item;
         });
         todo.push(...done);
-        this.setState({items: todo});
+        //this.setState({items: todo});
         localStorage.setItem('items', JSON.stringify(this.state.items));
     }
 
@@ -61,13 +88,16 @@ export class ToDoList extends React.Component {
         return(
             <div className="todolist">
                 <h2>ToDo List</h2>
+                <form>
+                    <input className="ipt searchIpt" placeholder="Search item..." type="text" value={this.state.searchVal} onChange={async e =>this.searchValUpdate(e.target.value)}/>
+                </form>
                 <div className="todolist-list">
-                    {this.state.items.map((item: any, idx: number) => {
-                        return <Item id={idx+1} name={item.name} checked={item.checked} cbClick={val => {this.updateCheckItem(idx, val)}}/>;
+                    {this.state.filteredItems.map((item: any, idx: number) => {
+                        return <Item key={item.id} id={item.id} name={item.name} checked={item.checked} cbClick={val => {this.updateCheckItem(item.id, val)}}/>;
                     })}
                 </div>
                 <form>
-                    <input className="textIpt" placeholder="New item..." type="text" value={this.state.inputVal} onChange={e =>this.setInputVal(e.target.value)}/>
+                    <input className="ipt textIpt" placeholder="New item..." type="text" value={this.state.inputVal} onChange={e =>this.setInputVal(e.target.value)}/>
                     <button className="btn" type="submit" onClick={this.addItem}>+</button>
                 </form>
             </div>
